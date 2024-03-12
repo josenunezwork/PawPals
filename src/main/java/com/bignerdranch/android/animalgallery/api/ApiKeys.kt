98021342e3691fe589430317b3df100985267ab1
josenunezwork <143661072+josenunezwork@users.noreply.github.com>
2024-03-12 16:53:04 -0700

    Update ApiKeys.kt

--- src/main/java/com/bignerdranch/android/animalgallery/api/ApiKeys.kt
+++ src/main/java/com/bignerdranch/android/animalgallery/api/ApiKeys.kt
@@ -5,7 +5,7 @@ class ApiKeys {
 
     //Yes, I know this is horrible.
     companion object {
-        const val API_KEY = "BWEhYdetarc7GWo39zybSQP2Hmh2MbIEjLZtGwX4BqhyuajKSu"
-        const val API_SECRET = "scodvKvqfLgPRCI7YkBSTURJAnVH0j0DGuQB0Mgc"
+        const val API_KEY = "my_key"
+        const val API_SECRET = "my_key"
     }
 }
